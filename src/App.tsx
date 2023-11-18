9a10eb761bda1af72e2e253f2ddcb0290a0a5e2d
jim.chen <azx011014@gmail.com>
2023-11-18 17:13:53 +0800

    [feature]: add sign-in and home routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 
 const App = () => {
-  return <main className="flex h-screen"></main>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
