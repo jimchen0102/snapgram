b57efd05b839d323a9f284bb2ea0827b2a041ed5
jim.chen <azx011014@gmail.com>
2023-11-18 17:46:15 +0800

    [feature]: add public resources

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-col flex-1 justify-center items-center py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden w-1/2 h-screen object-cover xl:block"
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
