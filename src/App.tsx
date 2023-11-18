e2e6519ed7f0ce5a903e28ad76b168d16dafd547
jim.chen <azx011014@gmail.com>
2023-11-18 17:19:37 +0800

    [feature]: add other pages and layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 import "./globals.css";
 
 const App = () => {
@@ -6,10 +12,15 @@ const App = () => {
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Route path="/sign-in" element={<SigninForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
         {/* private routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
